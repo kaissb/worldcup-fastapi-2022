2e58bc53b1e04f4100891b778ecc49af5ff3439b
kaissb <59128181+kaissb@users.noreply.github.com>
2022-12-03 11:18:18 +0100

    added round of 16

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get('/')
+@app.get("/")
+async def test(request: Request):
+
+    context = {
+        "request": request,
+        "_matches": matches().json(),
+        "_matches_current": matches_current().json(),
+        "_matches_today": matches_today().json(),
+        "_teams": teams().json(),
+    }
+    return templates.TemplateResponse("test.html", context)
+
+
+@app.get("/")
 async def index(request: Request):
     context = {
         "request": request,
@@ -21,7 +34,7 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", context)
 
 
-@app.get('/team/{code}')
+@app.get("/team/{code}")
 async def team_view(request: Request, code: str):
     team_data = team(code).json()
 
